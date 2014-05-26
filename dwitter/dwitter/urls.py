a73d6e752aaac09b7781dd1759b6eaf8bbde5c0f
Juan Riaza <github@juanriaza.com>
2014-05-26 21:22:03 +0200

    Implement individual page for a tweet

--- dwitter/dwitter/urls.py
+++ dwitter/dwitter/urls.py
@@ -9,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^tweet/(\d+)/$', 'website.views.tweet_page'),
     url(r'^$', 'website.views.timeline'),
 )
