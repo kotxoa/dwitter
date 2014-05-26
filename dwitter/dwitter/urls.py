bc89b1c4ea5d6682d887d80e5fce82949a2ec9f9
Juan Riaza <github@juanriaza.com>
2014-05-26 21:54:34 +0200

    Use reverse for displaying urls

--- dwitter/dwitter/urls.py
+++ dwitter/dwitter/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^tweet/(\d+)/$', 'website.views.tweet_page'),
-    url(r'^user/(\w+)/$', 'website.views.user_page'),
-    url(r'^$', 'website.views.timeline'),
+    url(r'^tweet/(\d+)/$', 'website.views.tweet_page', name='tweet-page'),
+    url(r'^user/(\w+)/$', 'website.views.user_page', name='user-page'),
+    url(r'^$', 'website.views.timeline', name='timeline'),
 )
