fde1b3a20c359a2ed33108400530461bcca4a38b
Juan Riaza <github@juanriaza.com>
2014-05-26 22:07:27 +0200

    Create login/logout views

--- dwitter/dwitter/urls.py
+++ dwitter/dwitter/urls.py
@@ -8,6 +8,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'dwitter.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^login/', 'django.contrib.auth.views.login', {'template_name': 'website/login.html'}, name='login'),
+    url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tweet/(\d+)/$', 'website.views.tweet_page', name='tweet-page'),
     url(r'^user/(\w+)/$', 'website.views.user_page', name='user-page'),
